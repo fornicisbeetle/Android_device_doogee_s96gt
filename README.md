a701dfc255af8cbbdaeb0af8deee323bb470f56b
fornicisbeetle <110208131+fornicisbeetle@users.noreply.github.com>
2023-03-06 19:09:04 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for DOOGEE S96GT (S96Pro)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo twrp_S96Pro-user
+add_lunch_combo twrp_S96Pro-userdebug
+add_lunch_combo twrp_S96Pro-eng
