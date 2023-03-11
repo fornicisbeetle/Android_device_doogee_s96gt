6e4c87690642f8fe402ffc59d52ac3e0f1b892cd
lopestorm <guillopes2002@gmail.com>
2023-03-11 12:38:53 -0300

    Update twrp_S96Pro.mk

--- twrp_S96Pro.mk
+++ twrp_S96Pro.mk
@@ -7,7 +7,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
